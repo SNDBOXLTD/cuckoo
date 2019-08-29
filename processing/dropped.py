e677ce73b3b9d5607d8c2fe24d3b09b8883ac7a1
Tomer Frid <tomer@sndbox.com>
2019-08-29 18:03:03 +0300

    whitelist dropped files

--- processing/dropped.py
+++ processing/dropped.py
@@ -5,12 +5,43 @@
 
 import json
 import os
+import re
+import logging
 
 from cuckoo.common.abstracts import Processing
 from cuckoo.common.objects import File
 
+logger = logging.getLogger(__name__)
+
+
 class Dropped(Processing):
     """Dropped files analysis."""
+
+    def _is_whitelisted_path(self, file_path):
+        """Exclude common whitelist files.
+        """
+        whitelist_paths = [
+            '\Users\Petra\AppData\Roaming\Microsoft\UProof\ExcludeDictionary',
+            '\Users\Petra\AppData\Local\Temp\~$',
+            '\Users\Petra\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.Word\~',
+            '\Users\Petra\AppData\Roaming\Microsoft\Publisher Building Blocks\ContentStore.xml',
+            '\Users\Petra\AppData\Local\Microsoft\Office\ONetConfig',
+            '\Users\Petra\AppData\LocalLow\Microsoft\CryptnetUrlCache\Content',
+            '\Users\Petra\AppData\LocalLow\Microsoft\CryptnetUrlCache\MetaData',
+            '\Windows\System32\winevt\Logs',
+        ]
+        whitelist_regex_patterns = [
+            r'\\Users\\Petra\\AppData\\Local\\Temp\\[0-9]+.*\.cvr',
+            r'\\Users\\Petra\\AppData\\Local\\Microsoft\\Windows\\Temporary Internet Files\\Content.MSO\\[0-9a-fA-F]+.(emf|wmf|dat)',
+            r'\\Users\\Petra\\AppData\\Local\\Temp\\\w+.tmp.WERInternalMetadata.xml',
+            r'\\Users\\Petra\\AppData\\Local\\Temp\\(Word8.0|Excel8.0|VBE)\\.*.exd',
+            r'\\Users\\Petra\\AppData\\Roaming\\Microsoft\\Forms\\.*.exd',
+            r'\\Users\\Petra\\AppData\\Roaming\\Microsoft\\Office\\.*\.xml',
+        ]
+
+        paths_test = all(path not in file_path for path in whitelist_paths)
+        regex_test = any(re.match(query, file_path) for query in whitelist_regex_patterns)
+        return paths_test and not regex_test
 
     def run(self):
         """Run analysis.
@@ -41,4 +72,6 @@ class Dropped(Processing):
                 file_info = File(file_path=file_path).get_all()
                 dropped_files.append(file_info)
 
-        return dropped_files
+        whitelisted_dropped_files = [f for f in dropped_files if self._is_whitelisted_path(f['filepath'])]
+
+        return whitelisted_dropped_files
