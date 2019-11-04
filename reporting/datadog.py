121272625e56403d7804ef2679e9622e6fbc2e24
Itay Hury <itay+git@huri.biz>
2019-11-04 18:57:22 +0200

    Add docs

--- reporting/datadog.py
+++ reporting/datadog.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2012-2013 Claudio Guarnieri.
-# Copyright (C) 2014-2017 Cuckoo Foundation.
-# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-# See the file 'docs/LICENSE' for copying permission.
-
 from ddtrace.propagation.http import HTTPPropagator
 from cuckoo.common.abstracts import Report
 from datetime import datetime
@@ -13,17 +8,26 @@ import json
 import math
 import os
 
-
 logger = logging.getLogger(__name__)
 
 
 class DataDog(Report):
+    """
+    Ships execution metrics and logs to Datadog.
+    Those traces will be associated with the trace of the upload.
+    """
     order = 6
 
     @staticmethod
     def _to_timestamp(date):
+        """
+        Converts a date to a timestamp
+        :param date: string, for example 2019-11-04T11:57:07.368053
+        :return: int, epoch timestamp in seconds
+        """
         epoch = datetime(1970, 1, 1)
-        return int(math.ceil((date - epoch).total_seconds()))
+        parsed_date = dateutil.parser.parse(date)
+        return int(math.ceil((parsed_date - epoch).total_seconds()))
 
     def run(self, results):
         tracer.configure(hostname=self.options.agent_host, port=self.options.agent_port)
@@ -40,19 +44,15 @@ class DataDog(Report):
         context = propagator.extract(custom.get("trace"))
         tracer.context_provider.activate(context)
 
-        # VM trace
+        # VM trace, starting from the time the task created and ending in the time the VM was shut down
         vm_span = tracer.start_span('cuckoo.analysis.vm', service=self.options.service_name, child_of=context)
-
-        start_time = dateutil.parser.parse(task["added_on"]["$dt"])
-        vm_span.start = self._to_timestamp(start_time)
-
-        finish = dateutil.parser.parse(task["completed_on"]["$dt"])
-        finish_timestamp = self._to_timestamp(finish)
+        vm_span.start = self._to_timestamp(task["added_on"]["$dt"])
+        finish_timestamp = self._to_timestamp(task["completed_on"]["$dt"])
         vm_span.finish(finish_timestamp)
 
-        # Reporting trace
+        # Reporting trace, VM shutdown - now
         context = propagator.extract(custom.get("trace"))
-        reporting_span = tracer.start_span('cuckoo.analysis.reporting', service=self.options.service_name, child_of=context)
+        reporting_span = tracer.start_span('cuckoo.analysis.reporting', service=self.options.service_name,
+                                           child_of=context)
         reporting_span.start = finish_timestamp
         reporting_span.finish()
-
